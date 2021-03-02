75f365adc8177fbe9700ec33bfe5b5b61a042698
Shane Forde <13754903+Dunmersc2@users.noreply.github.com>
2021-03-02 20:58:13 +0000

    Day zero code

---
+++ app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!\n'
+
+
+@app.route('/books')
+def display_books():
+    books = [
+        {
+            "author": "Shaw, Zed",
+            "rating": "3.92",
+            "image": "ef0ceaab-32a8-47fb-ba13-c0b362d970da.jpg"
+        },
+        {
+            "author": "Shaw, Zed",
+            "rating": "3.92",
+            "image": "ef0ceaab-32a8-47fb-ba13-c0b362d970da.jpg"
+        },
+        {
+            "author": "Shaw, Zed",
+            "rating": "3.92",
+            "image": "ef0ceaab-32a8-47fb-ba13-c0b362d970da.jpg"
+        },
+        {
+            "author": "Shaw, Zed",
+            "rating": "3.92",
+            "image": "ef0ceaab-32a8-47fb-ba13-c0b362d970da.jpg"
+        },
+        {
+            "author": "Shaw, Zed",
+            "rating": "3.92",
+            "image": "ef0ceaab-32a8-47fb-ba13-c0b362d970da.jpg"
+        }
+    ]
+    return render_template("index.htm", books=books)
